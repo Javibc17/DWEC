b5df9b3b94e32a6c8b68d2182eb1160a068b2a8e
Javibc17 <javibece1717@alumnos.alborfp.com>
2024-11-28 14:11:54 +0100

    0.1

---
+++ script.js
@@ -0,0 +1,62 @@
+class Viaje {
+    constructor(codigo, destino, precio, disponibilidad = true) {
+        this.codigo = codigo;
+        this.destino = destino;
+        this.precio = precio;
+        this.disponibilidad = disponibilidad;
+    }
+    getInfo() {
+        return `Viaje [${this.codigo}] a ${this.destino}, precio: ${this.precio} euros`;
+    }
+}
+
+class Vuelo extends Viaje {
+    constructor(codigo, destino, precio, aerolinea, duracion) {
+        super(codigo, destino, precio);
+        this.aerolinea = aerolinea;
+        this.duracion = duracion;
+    }
+    getInfo() {
+        return `${super.getInfo()}, Aerolínea: ${this.aerolinea}, Duración: ${this.duracion} horas`;
+    }
+}
+
+class Hotel extends Viaje {
+    constructor(codigo, destino, precio, vuelo, hotel) {
+        super(codigo, destino, precio);
+        this.vuelo = vuelo;
+        this.hotel = hotel;
+    }
+    getInfo() {
+        return `${super.getInfo()}\n - Vuelo: ${this.vuelo.getInfo()}\n - Hotel: ${this.hotel.getInfo()}`;
+    }
+}
+
+class Cliente {
+    constructor(nombre, apellido, email, telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+    getResumen() {
+        return `Cliente: ${this.nombre} ${this.apellido}, Email: ${this.email}, Teléfono: ${this.telefono}`;
+    }
+}
+
+class Reserva {
+    constructor(cliente, viaje, fecha) {
+        this.cliente = cliente;
+        this.viaje = viaje;
+        this.fecha = fecha;
+    }
+    getResumen() {
+        return `${this.cliente.getResumen()}\nReservó: ${this.viaje.getInfo()}`;
+    }
+}
+
+const vuelo1 = new Viaje("jk27", "Paris", 500)
+
+
+
+
